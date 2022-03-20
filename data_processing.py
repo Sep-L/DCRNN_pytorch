045874463b6c25f04348a5257ee9bb659ac608be
LQZ <907877767@qq.com>
2022-03-20 09:10:26 +0800

    first commit

---
+++ data_processing.py
@@ -0,0 +1,48 @@
+"""
+DCRNN
+Description: 数据预处理
+Author: LQZ
+Time: 2022/3/18 16:26 
+"""
+
+
+def generate_graph_seq2seq_io_data(data_frame, x_offsets, y_offsets, add_time_in_day=True, add_day_in_week=False, scaler=None):
+    """
+    Generate samples from
+    :param data_frame: 读取的数据
+    :param x_offsets:
+    :param y_offsets:
+    :param add_time_in_day:
+    :param add_day_in_week:
+    :param scaler:
+    :return:
+    # x: (epoch_size, input_length, num_nodes, input_dim)
+    # y: (epoch_size, output_length, num_nodes, output_dim)
+    """
+    # num_samples 样本总数, num_nodes 节点数
+    num_samples, num_nodes = data_frame.shape
+    data = np.expand_dims(df.values, axis=-1)
+    data_list = [data]
+    if add_time_in_day:
+        time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
+        time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
+        data_list.append(time_in_day)
+    if add_day_in_week:
+        day_in_week = np.zeros(shape=(num_samples, num_nodes, 7))
+        day_in_week[np.arange(num_samples), :, df.index.dayofweek] = 1
+        data_list.append(day_in_week)
+
+    data = np.concatenate(data_list, axis=-1)
+    # epoch_len = num_samples + min(x_offsets) - max(y_offsets)
+    x, y = [], []
+    # t is the index of the last observation.
+    min_t = abs(min(x_offsets))
+    max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
+    for t in range(min_t, max_t):
+        x_t = data[t + x_offsets, ...]
+        y_t = data[t + y_offsets, ...]
+        x.append(x_t)
+        y.append(y_t)
+    x = np.stack(x, axis=0)
+    y = np.stack(y, axis=0)
+    return x, y
